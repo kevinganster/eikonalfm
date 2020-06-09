c5f45b77fdf6a4e61156390b5c1b0966af89194e
Kevin Ganster <kevin.ganster@kit.edu>
2020-06-09 23:54:06 +0200

    distance function adjustments

--- eikonalfm/__init__.py
+++ eikonalfm/__init__.py
@@ -7,7 +7,7 @@ eikonalfm is a Python (C++-)extension which implements the fast marching method 
 and the factored eikonal equation
     | (tau0 grad(tau1) + tau1 grad(tau0))(x) |^2 = 1 / c^2(x).
 
-See https://github.com/Daarknes/eikonalfm for more information
+See https://github.com/kevinganster/eikonalfm for more information
 """
 __version__ = "0.9.3"
 
@@ -15,7 +15,7 @@ import numpy as np
 from .cfm import fast_marching, factored_fast_marching
 
 
-def distance(shape, dx, x_s, indexing="ij"):
+def distance(shape, dx, x_s, indexing="xy"):
     """
     Calculates a distance field according to the euclidian metric.
 
@@ -29,12 +29,17 @@ def distance(shape, dx, x_s, indexing="ij"):
         dx : sequence of doubles
             Grid spacing for each dimension, dx > 0.
             Must have the same length as the number of dimensions of shape.
+        indexing : {'xy', 'ij'}, optional
+            Cartesian ('xy', default) or matrix ('ij') indexing of output.
+            See numpy's meshgrid function for more details.
 
     Returns
     ----------
         tau0 : ndarray
             euclidian distance field |x - x_s|.
     """
+    # TODO: maybe change x_s to index-vector to be consistent
+    assert(len(shape) == len(dx) == len(x_s))
     x = []
     for shape_i, dx_i in zip(shape, dx):
         x.append(np.linspace(0, shape_i*dx_i, shape_i, endpoint=False))
