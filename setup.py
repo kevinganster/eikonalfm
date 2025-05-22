069b24f7b693e8176dd977834da66cac5d0d0beb
Kevin Ganster <kevinganster@gmail.com>
2025-05-22 16:37:37 +0200

    fix workflow; license for older setuptools versions; fix -std=c++11 warning; Python < 3.7 no longer supported (maybe soon also < 3.9), but 3.12 and 3.13 should now work.

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
+import sys
 from setuptools import setup, Extension
 import numpy as np
 
 
-pkg_metadata = {}
-pkg_metadata['ext_modules'] = [
-    Extension(
-        "eikonalfm.cfm",
-        sources=[   "src/eikonalfm/cfm.cpp",
-                    "src/eikonalfm/marcher.cpp",
-                    "src/eikonalfm/factoredmarcher.cpp",
-                    "src/eikonalfm/heap.cpp"],
-        include_dirs=[  "src/eikonalfm",
-                        np.get_include()],
-        language="c++",
-        extra_compile_args=["-std=c++11"]  # god damn it Mac OS
-    )
-]
-setup(**pkg_metadata)
+ext_args = {}
+if sys.platform == "darwin":
+    ext_args['extra_compile_args'] = ["-std=c++11"] # god damn it Mac OS
+
+extension = Extension(
+    "eikonalfm.cfm",
+    sources=[   "src/eikonalfm/cfm.cpp",
+                "src/eikonalfm/marcher.cpp",
+                "src/eikonalfm/factoredmarcher.cpp",
+                "src/eikonalfm/heap.cpp"],
+    include_dirs=[  "src/eikonalfm",
+                    np.get_include()],
+    language="c++",
+    **ext_args
+)
+
+setup(
+    ext_modules = [extension],
+    license = "MIT"
+)
